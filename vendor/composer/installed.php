0c287ad33a3bf5c4ff994e07e78df8667dbc79b9
ArielAnza <arielanza2006@gmail.com>
2025-09-13 11:43:55 -0300

    Subiendo 1era version

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'f8f75993bb0d4d98638fd1c76b9ad3bf2a1e78f7',
+        'name' => 'mini-framework/mvc',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'mini-framework/mvc' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'f8f75993bb0d4d98638fd1c76b9ad3bf2a1e78f7',
+            'dev_requirement' => false,
+        ),
+    ),
+);
