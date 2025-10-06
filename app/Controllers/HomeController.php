9aecff795b1873155f5aaed369e36cf58ee8415c
root <root@localhost.localdomain>
2025-10-06 12:01:52 -0300

    ultimos cambios

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -7,7 +7,11 @@ class HomeController extends Controller {
     public function index() {
         $userModel=new \App\Models\User(); 
         $datos=$this->session->get("user");
+	$rutAdmin = null;
+	if ($datos && isset($datos['rut'])) {
+
         $rutAdmin=$userModel->findAdmin($datos['rut']) ;
+}
         return $this->render('home', [
             'title' => 'SAMA Solutions',
             'rutAdmin' => $rutAdmin,
